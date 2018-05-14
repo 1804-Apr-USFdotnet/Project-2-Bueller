fa17d295fa44656bbc15ded7e3748abef0741220
Rafael Berger <RafiBerger613@gmail.com>
2018-05-14 17:07:47 -0400

    Added Account

--- Project2-Bueller/Bueller.DA/Models/Account.cs
+++ Project2-Bueller/Bueller.DA/Models/Account.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Bueller.DA.Models
 {
+    [Table("Account", Schema = "Billing")]
     public class Account : BaseEntity
     {
+
+        public int AccountId { get; set; }
+        public double? BalanceOwed { get; set; }
+        public double? Aid { get; set; }
+        public double? TotalExpense { get; set; }
+        public string PayPeriod { get; set; }
+        public double? Salary { get; set; }
     }
 }
