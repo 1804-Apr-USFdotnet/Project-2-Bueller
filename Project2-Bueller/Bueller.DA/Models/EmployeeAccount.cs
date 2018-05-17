f139805fcafac088d48e67e695784c112a9b990a
Manjot Chahal <mchahal22@gmail.com>
2018-05-17 14:16:25 -0400

    finished student and employee models

--- Project2-Bueller/Bueller.DA/Models/EmployeeAccount.cs
+++ Project2-Bueller/Bueller.DA/Models/EmployeeAccount.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Bueller.DA.Models
 {
-    class EmployeeAccount
+    [Table("EmployeeAccounts", Schema = "Accounts")]
+    public class EmployeeAccount
     {
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [ScaffoldColumn(false)]
+        public int EmployeeAccountId { get; set; }
+
+        //weekly, bi-weekly, monthly
+        public string PayPeriod { get; set; }
+
+        [DataType(DataType.Currency)]
+        public double Salary { get; set; }
+
+        public DateTime Created { get; set; }
+        public DateTime Modified { get; set; }
     }
 }
--- Project2-Bueller/Bueller.DA/Models/StudentAccount.cs
+++ Project2-Bueller/Bueller.DA/Models/StudentAccount.cs
@@ -8,30 +8,23 @@ using System.Threading.Tasks;
 
 namespace Bueller.DA.Models
 {
-    [Table("Accounts", Schema = "Billing")]
+    [Table("StudentAccounts", Schema = "Accounts")]
     public class StudentAccount : BaseEntity
     {
         [Key]
         [Required]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [ScaffoldColumn(false)]
-        public int AccountId { get; set; }
-
+        public int StudentAccountId { get; set; }
  
         [DataType(DataType.Currency)]
-        public double? BalanceOwed { get; set; }
+        public double BalanceOwed { get; set; }
 
         [DataType(DataType.Currency)]
-        public double? Aid { get; set; }
+        public double Aid { get; set; }
 
         [DataType(DataType.Currency)]
-        public double? TotalExpense { get; set; }
-
-        //weekly, bi-weekly, monthly
-        public string PayPeriod { get; set; }
-
-        [DataType(DataType.Currency)]
-        public double? Salary { get; set; }
+        public double TotalExpense { get; set; }
 
         public DateTime Created { get; set; }
         public DateTime Modified { get; set; }
