67543353829d9b132cfa020f29fdab66d9be0b5e
Rafael Berger <RafiBerger613@gmail.com>
2018-05-17 10:38:40 -0400

    commiting before pull

---
+++ Project2-Bueller/Bueller.DA/Models/EmployeePayment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bueller.DA.Models
+{
+   public  class EmployeePayment : BaseEntity
+    {
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [ScaffoldColumn(false)]
+        public int EmployeePaymentId { get; set; }
+        [ScaffoldColumn(false)]
+        public int EmployeeId { get; set;}
+        [ForeignKey("EmployeeId")]
+        public virtual Employee Employee { get; set; }
+
+        public double? PaymentAmount { get; set; }
+        
+
+        public DateTime Created { get; set; }
+        public DateTime Modified { get; set; }
+
+    }
+}
---
+++ Project2-Bueller/Bueller.DA/Models/EmployeeSalary.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bueller.DA.Models
+{ [Table("EmployeeAccounts", Schema ="Employee")]
+     public class EmployeeSalary : BaseEntity
+    {
+
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [ScaffoldColumn(false)]
+        public int EmployeeSalaryId{ get; set; }
+
+        public double Salary  { get; set; }
+        [ScaffoldColumn(false)]
+        public int EmployeeId { get; set; }
+        [ForeignKey("EmployeeId")]
+        public virtual Employee Employee { get; set; }
+
+
+        public DateTime Created { get; set; }
+        public DateTime Modified { get; set; }
+    }
+}
