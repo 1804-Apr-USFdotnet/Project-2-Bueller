255d40361b2df88df58863a00985ae97c5f5cca1
Rafael Berger <RafiBerger613@gmail.com>
2018-05-17 19:56:39 -0400

    Scafolded Mvc template

---
+++ Project2-BuellerWebSln/Project2-BuellerWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Project2_BuellerWeb.Startup))]
+namespace Project2_BuellerWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
