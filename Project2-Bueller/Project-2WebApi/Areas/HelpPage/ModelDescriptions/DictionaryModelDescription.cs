1300778f3114c34cc1d1352522ffcd20574f1f30
Rafael Berger <RafiBerger613@gmail.com>
2018-05-17 20:30:23 -0400

    Added  WebApi project

---
+++ Project2-Bueller/Project-2WebApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Project_2WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Project2-Bueller/Project-2WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Project_2WebApi.WebApiApplication" Language="C#" %>
