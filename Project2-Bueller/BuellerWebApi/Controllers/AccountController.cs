b18db316f0410a3ab7d100859058d7d3b205e319
Rafael Berger <RafiBerger613@gmail.com>
2018-05-20 10:35:53 -0400

    Added Identity

--- Project2-Bueller/BuellerWebApi/Controllers/AccountController.cs
+++ Project2-Bueller/BuellerWebApi/Controllers/AccountController.cs
@@ -1,115 +1,117 @@
-﻿//using Bueller.DA;
-//using BuellerWebApi.Models;
-//using Microsoft.AspNet.Identity;
-//using Microsoft.AspNet.Identity.EntityFramework;
-//using Microsoft.Owin.Security;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Net;
-//using System.Net.Http;
-//using System.Security.Claims;
-//using System.Web.Http;
-
-//namespace BuellerWebApi.Controllers
-//{
-//    public class AccountController : ApiController
-//    {
+﻿using Bueller.DA;
+using BuellerWebApi.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.Owin.Security;
+using System.Security.Claims;
+using Microsoft.Owin.Host.SystemWeb;
 
 
-//        [HttpPost]
-//        [Route("~/api/Account/Register")]
-//        [AllowAnonymous]
-//        public IHttpActionResult Register(Account account)
-//        {
-//            if (!ModelState.IsValid)
-//            {
-//                return BadRequest();
-//            }
-
-//            // actually register
-//            var userStore = new UserStore<IdentityUser>(new BuellerContext());
-//            var userManager = new UserManager<IdentityUser>(userStore);
-//            var user = new IdentityUser(account.UserName);
-
-//            if (userManager.Users.Any(u => u.UserName == account.UserName))
-//            {
-//                return BadRequest();
-//            }
-
-//            userManager.Create(user, account.Password);
-
-//            return Ok();
-//        }
-
-//        [HttpPost]
-//        [Route("~/api/Account/RegisterAdmin")]
-//        [AllowAnonymous]
-//        public IHttpActionResult RegisterAdmin(Account account)
-//        {
-//            if (!ModelState.IsValid)
-//            {
-//                return BadRequest();
-//            }
-
-//            // actually register
-//            var userStore = new UserStore<IdentityUser>(new BuellerContext());
-//            var userManager = new UserManager<IdentityUser>(userStore);
-//            var user = new IdentityUser(account.UserName);
-
-//            if (userManager.Users.Any(u => u.UserName == account.UserName))
-//            {
-//                return BadRequest();
-//            }
-
-//            userManager.Create(user, account.Password);
-
-//            // the only difference from Register action
-//            userManager.AddClaim(user.Id, new Claim(ClaimTypes.Role, "admin"));
-
-//            return Ok();
-//        }
-
-//        [HttpPost]
-//        [Route("~/api/Account/Login")]
-//        [AllowAnonymous]
-//        public IHttpActionResult LogIn(Account account)
-//        {
-//            if (!ModelState.IsValid)
-//            {
-//                return BadRequest();
-//            }
-
-//            // actually login
-//            var userStore = new UserStore<IdentityUser>(new BuellerContext());
-//            var userManager = new UserManager<IdentityUser>(userStore);
-//            var user = userManager.Users.FirstOrDefault(u => u.UserName == account.UserName);
+namespace BuellerWebApi.Controllers
+{
+    public class AccountController : ApiController
+    {
 
 
-//            if (user == null)
-//            {
-//                return BadRequest();
-//            }
+        [HttpPost]
+        [Route("~/api/Account/Register")]
+        [AllowAnonymous]
+        public IHttpActionResult Register(Account account)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
 
-//            if (!userManager.CheckPassword(user, account.Password))
-//            {
-//                return Unauthorized();
-//            }
+            // actually register
+            var userStore = new UserStore<IdentityUser>(new BuellerContext());
+            var userManager = new UserManager<IdentityUser>(userStore);
+            var user = new IdentityUser(account.Email);
 
-//            var authManager = Request.GetOwinContext().Authentication;
-//            var claimsIdentity = userManager.CreateIdentity(user, "ApplicationCookie");
+            if (userManager.Users.Any(u => u.UserName == account.Email))
+            {
+                return BadRequest();
+            }
 
-//            authManager.SignIn(new AuthenticationProperties { IsPersistent = true }, claimsIdentity);
+            userManager.Create(user, account.Password);
 
-//            return Ok();
-//        }
+            return Ok();
+        }
 
-//        [HttpGet]
-//        [Route("~/api/Account/Logout")]
-//        public IHttpActionResult Logout()
-//        {
-//            Request.GetOwinContext().Authentication.SignOut("ApplicationCookie");
-//            return Ok();
-//        }
-//    }
-//}
+        [HttpPost]
+        [Route("~/api/Account/RegisterAdmin")]
+        [AllowAnonymous]
+        public IHttpActionResult RegisterAdmin(Account account)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            // actually register
+            var userStore = new UserStore<IdentityUser>(new BuellerContext());
+            var userManager = new UserManager<IdentityUser>(userStore);
+            var user = new IdentityUser(account.Email);
+
+            if (userManager.Users.Any(u => u.UserName == account.Email))
+            {
+                return BadRequest();
+            }
+
+            userManager.Create(user, account.Password);
+
+            // the only difference from Register action
+            userManager.AddClaim(user.Id, new Claim(ClaimTypes.Role, "admin"));
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("~/api/Account/Login")]
+        [AllowAnonymous]
+        public IHttpActionResult LogIn(Account account)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            // actually login
+            var userStore = new UserStore<IdentityUser>(new BuellerContext());
+            var userManager = new UserManager<IdentityUser>(userStore);
+            var user = userManager.Users.FirstOrDefault(u => u.UserName == account.Email);
+
+
+            if (user == null)
+            {
+                return BadRequest();
+            }
+
+            if (!userManager.CheckPassword(user, account.Password))
+            {
+                return Unauthorized();
+            }
+
+            var authManager = Request.GetOwinContext().Authentication;
+            var claimsIdentity = userManager.CreateIdentity(user, "ApplicationCookie");
+
+            authManager.SignIn(new AuthenticationProperties { IsPersistent = true }, claimsIdentity);
+
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("~/api/Account/Logout")]
+        public IHttpActionResult Logout()
+        {
+            Request.GetOwinContext().Authentication.SignOut("ApplicationCookie");
+            return Ok();
+        }
+    }
+}
--- Project2-Bueller/BuellerWebApi/Models/Account.cs
+++ Project2-Bueller/BuellerWebApi/Models/Account.cs
@@ -7,7 +7,7 @@ namespace BuellerWebApi.Models
 {
     public class Account
     {
-        public string FristName { get; set; }
+        public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
