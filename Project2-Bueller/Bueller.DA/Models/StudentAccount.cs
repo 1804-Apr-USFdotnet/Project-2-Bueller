5e93c852de23a491442693b5b84aebeee223aa3f
Keenan Baynard <ksbaynard@hotmail.com>
2018-05-20 18:02:51 -0400

    fix configuration

--- Project2-Bueller/Bueller.DA/Models/StudentAccount.cs
+++ Project2-Bueller/Bueller.DA/Models/StudentAccount.cs
@@ -15,9 +15,16 @@ namespace Bueller.DA.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [ScaffoldColumn(false)]
         public int StudentAccountId { get; set; }
- 
+
+        [NotMapped]
         [DataType(DataType.Currency)]
-        public double BalanceOwed { get; set; }
+        public double BalanceOwed
+        {
+            get
+            {
+                return TotalExpense - Aid;
+            }
+        }
 
         [DataType(DataType.Currency)]
         public double Aid { get; set; }
