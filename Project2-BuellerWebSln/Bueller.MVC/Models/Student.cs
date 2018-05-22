0a5e84c6a6163376476e3b0a2c7da8d316f71cd2
Manjot Chahal <mchahal22@gmail.com>
2018-05-22 15:16:40 -0400

    validations on register student info

--- Project2-BuellerWebSln/Bueller.MVC/Models/Student.cs
+++ Project2-BuellerWebSln/Bueller.MVC/Models/Student.cs
@@ -13,13 +13,16 @@ namespace Bueller.MVC.Models
         public int StudentId { get; set; }
         [Required(ErrorMessage = "First name is required")]
         [DataType(DataType.Text)]
+        [Display(Name = "First Name")]
         [StringLength(100, ErrorMessage = "First name cannot be more than 100 characters")]
         public string FirstName { get; set; }
         [DataType(DataType.Text)]
+        [Display(Name = "Middle Name")]
         [StringLength(100, ErrorMessage = "Middle name cannot be more than 100 characters")]
         public string MiddleName { get; set; }
         [Required(ErrorMessage = "Last name is required")]
         [DataType(DataType.Text)]
+        [Display(Name = "Last Name")]
         [StringLength(100, ErrorMessage = "Last name cannot be more than 100 characters")]
         public string LastName { get; set; }
 
@@ -44,6 +47,7 @@ namespace Bueller.MVC.Models
         [DataType(DataType.PostalCode)]
         public string Zipcode { get; set; }
         [RegularExpression("[(]{1}[0-9]{3}[)]{1}[ ]{1}[0-9]{3}[-]{1}[0-9]{4}", ErrorMessage = "Format must be (###) ###-####")]
+        [Display(Name = "Phone Number")]
         [DataType(DataType.PhoneNumber)]
         public string PhoneNumber { get; set; }
 
