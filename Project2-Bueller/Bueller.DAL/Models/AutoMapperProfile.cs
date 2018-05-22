e0dbfdb48eeea05ab15fe2dfe467b200c38107ca
Rafael Berger <RafiBerger613@gmail.com>
2018-05-22 09:33:31 -0400

    Merge

--- Project2-Bueller/Bueller.DAL/Models/AutoMapperProfile.cs
+++ Project2-Bueller/Bueller.DAL/Models/AutoMapperProfile.cs
@@ -15,6 +15,7 @@ namespace Bueller.DAL.Models
             CreateMap<Employee, EmployeeDto>().ReverseMap();
             CreateMap<EmployeeAccount, EmployeeAccountDto>().ReverseMap();
             CreateMap<File, FileDto>().ReverseMap();
+            CreateMap<Assignment, AssignmentDto>().ReverseMap();
         }
     }
 }
--- Project2-Bueller/Bueller.DAL/Repos/GradeRepo.cs
+++ Project2-Bueller/Bueller.DAL/Repos/GradeRepo.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Bueller.DAL.Repos
 {
-    class GradeRepo
+   public  class GradeRepo
     {
     }
 }
