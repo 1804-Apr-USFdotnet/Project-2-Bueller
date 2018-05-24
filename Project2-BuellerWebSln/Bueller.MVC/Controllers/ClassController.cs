03dc219ced58442f1088577968d126a046fe8641
Manjot Chahal <mchahal22@gmail.com>
2018-05-24 15:41:07 -0400

    view classes mvc client

---
+++ Project2-BuellerWebSln/Bueller.MVC/Controllers/ClassController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Bueller.MVC.Models;
+
+namespace Bueller.MVC.Controllers
+{
+    public class ClassController : AServiceController
+    {
+        // GET: Assignment
+        [HttpGet]
+        public async Task<ViewResult> Index()
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View("Error");
+            }
+
+            HttpRequestMessage apiRequest = CreateRequestToService(HttpMethod.Get, "api/Class/GetAll");
+
+            HttpResponseMessage apiResponse;
+            Assignment assignment = new Assignment();
+
+            try
+            {
+                apiResponse = await HttpClient.SendAsync(apiRequest);
+            }
+            catch
+            {
+                return View("Error");
+            }
+
+            if (!apiResponse.IsSuccessStatusCode)
+            {
+                return View("Error");
+            }
+
+
+            var classes = await apiResponse.Content.ReadAsAsync<List<Class>>();
+
+            return View(classes);
+        }
+    }
+}
