d301de00bef2cca9aca192febb09127f1c514f81
Manjot Chahal <mchahal22@gmail.com>
2018-05-26 15:33:09 -0400

    ^ follow up

--- Project2-Bueller/BuellerWebApi/Controllers/StudentController.cs
+++ Project2-Bueller/BuellerWebApi/Controllers/StudentController.cs
@@ -44,28 +44,6 @@ namespace BuellerWebApi.Controllers
             return Ok(students);
         }
 
-        [HttpGet]
-        [Route("GetLoginInfo")]
-        public IHttpActionResult GetLoginInfo()
-        {
-            // making use of global authorize filter in webapiconfig / filterconfig
-
-            // get the currently logged-in user
-            var user = Request.GetOwinContext().Authentication.User;
-
-            // get his username
-            string username = user.Identity.Name;
-
-            // get whether user has some role
-            bool isAdmin = user.IsInRole("admin");
-
-            // get all user's roles
-            //List<string> roles = user.Claims.Where(x => x.Type == ClaimTypes.Role).Select(x => x.Value.ToString()).ToList();
-            string role = user.Claims.Where(x => x.Type == ClaimTypes.Role).Select(x => x.Value.ToString()).FirstOrDefault();
-            //return Ok($"Authenticated {username}, with roles: [{string.Join(", ", roles)}]!");
-            return Ok($"Authenticated {username}/nRole: {role}");
-        }
-
         // GET: api/Student/5
         [HttpGet]
         [Route("GetById/{id}")]
